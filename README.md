dda4c5519293c818be29193ba4a039808d1ae618
Eduardo Cerqueira <ecerquei@redhat.com>
2022-07-30 01:13:30 -0400

    initial commit simple example using basic window GTK and notification objects

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# gtk_app
-Python app with GTK 
+# GTK APP
+
+Python application using PyGObject and GTK library for GUI, running as GNOME application.
+
+*It is an experimental application, for educational purpose.*
+
+## installation
+
+```shell
+sudo dnf install gtk4 gtk4-devel gcc gobject-introspection-devel cairo-gobject-devel pkg-config python3-devel gtk3
+
+```
+
+```shell
+python3 -m venv venv
+source /venv/bin/activate
+
+```
+
+## links and references
+* [python-gtk-3-tutorial](https://python-gtk-3-tutorial.readthedocs.io/en/latest)
+* [gtk.org/python](https://www.gtk.org/docs/language-bindings/python)
+* [docs.gtk.org/gtk4](https://docs.gtk.org/gtk4/)
+* [pygobject](https://pygobject.readthedocs.io/en/latest/)
+* [pygobject repo](https://gitlab.gnome.org/GNOME/pygobject/)
+* [GNOME HIG](https://developer.gnome.org/hig/)
---
+++ exemple_notification.py
@@ -0,0 +1,36 @@
+import gi
+from datetime import datetime
+
+gi.require_version("Gtk", "3.0")
+gi.require_version('Notify', '0.7')
+
+from gi.repository import Gtk
+from gi.repository import Notify
+
+_GTK_APP_TITLE = "GTK Python App"
+
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title=_GTK_APP_TITLE, window_position=Gtk.WindowPosition.CENTER_ALWAYS)
+        Gtk.Window.set_default_size(self, 640, 480)
+        Notify.init(_GTK_APP_TITLE)
+
+        self.box = Gtk.Box(spacing=6)
+        self.add(self.box)
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.set_halign(Gtk.Align.CENTER)
+        self.button.set_valign(Gtk.Align.CENTER)
+        self.button.connect("clicked", self.send_notification)
+        self.box.pack_start(self.button, True, True, 0)
+
+    def send_notification(self, widget):
+        n = Notify.Notification.new(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), _GTK_APP_TITLE, "*")
+        n.show()
+
+
+win = MainWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
